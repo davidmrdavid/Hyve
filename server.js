21a85c55f4f2bd7a9318254aefebe0bcea9a33c8
Purag Moumdjian <purmou@gmail.com>
2015-08-08 16:25:49 -0700

    finished index + prepping server for socket

--- server.js
+++ server.js
@@ -4,17 +4,11 @@ var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
+var backend        = require('app/backend.js');
+var socket         = require('app/io.js');
 
 var port = process.env.PORT || 8080; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
-
-// web sockets =============================================
-app.http().io();
-app.io.route('ready', function(req) {
-    app.io.broadcast('new visitor');
-    console.log('user connected');
-});
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
@@ -27,14 +21,6 @@ app.use(express.static(__dirname + '/public')); // set the static files location
 var Student=require('./config/dbMode.js').Student;
 var Class=require('./config/dbMode.js').Class;
 var Teacher=require('./config/dbMode.js').Teacher;
-
-var Bla= new Student({
-  studentName:"Joe"
-});
-Bla.save(function(err,data){
-  if(err) return console.log(err);
-  console.log(data);
-});
 
 // start app ===============================================
 app.listen(port);
